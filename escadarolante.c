76034e4eab4e1410ef70b9baa29eed0d31241663
Cloud User <ec2-user@ip-172-31-89-182.ec2.internal>
2023-11-19 00:47:32 +0000

    projeto

--- escadarolante.c
+++ escadarolante.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
+#define MAX_n 10000
+#define MAX_t 100000
+#define MAX_d 1
 
 int main() {
-    int n;
-    scanf("%d", &n); // Lê o número de pessoas
+    int n = 3; // Número de pessoas
+
+    int tempos[] = {5, 7, 9}; // Vetor de tempos (exemplo)
+    int posicoes[] = {0, 1, 0}; // Vetor de posições (exemplo)
 
     int tempo_atual = 0; // Tempo atual
     int direcao = 0; // Direção inicial (0 para direita, 1 para esquerda)
     int tempo_espera = 0; // Tempo de espera
 
     for (int i = 0; i < n; i++) {
-        int t, d; // Momento de chegada e direção (0 ou 1)
-        scanf("%d %d", &t, &d);
+        int t = tempos[i];
+        int d = posicoes[i];
 
+        // Verifica as direções
         if (direcao == d) {
-            // Se a direção da pessoa é a mesma que a direção atual, não precisa esperar
             tempo_espera = 0;
         } else {
-            // Se a direção da pessoa é oposta à direção atual
-            // A pessoa deve esperar até que a escada pare e mude de direção
             tempo_espera = 10;
-            direcao = d; // Atualiza a direção
+            direcao = d;
         }
 
-        int tempo_mover = t - tempo_atual;
-        if (tempo_mover < 0) {
-            // Se a pessoa chegou antes da escada parar, atualiza o tempo para o momento em que a escada para
+        // Calcula o tempo necessário para se mover na escada e
+        // gerencia o tempo de espera quando a direção da pessoa muda
+        int time_to_move = t - tempo_atual;
+        if (time_to_move < 0) {
             tempo_atual = t;
         } else {
-            tempo_atual += tempo_mover; // Move-se até o momento de chegada da pessoa
+            tempo_atual += time_to_move;
         }
 
-        tempo_atual += tempo_espera; // Adiciona o tempo de espera
-        tempo_atual += 10; // Leva 10 segundos para a pessoa sair da escada
+        tempo_atual += tempo_espera;
+        tempo_atual += 10;
     }
 
     printf("%d\n", tempo_atual);
